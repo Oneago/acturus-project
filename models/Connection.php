0914e5ca3d7023294d7cd605ba188487dc7a2a6d
Juan David Rueda Quiroga <jrueda@oneago.com>
2021-03-16 00:17:44 -0500

    Closed #4

--- models/Connection.php
+++ models/Connection.php
@@ -3,23 +3,29 @@
 
 namespace App\Models;
 
-use mysqli;
+use Exception;
+use PDO;
 
 abstract class Connection
 {
-    /**
-     * @var string
-     */
-    protected $errorDetails;
+    protected string $errorDetails;
 
     /**
-     * @return mysqli database connection
+     * @return PDO database connection
      */
-    protected static function init(): mysqli
+    protected static function init(): PDO
     {
-        $con = mysqli_connect($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASS"], $_ENV["DB_NAME"], $_ENV["DB_PORT"] ?? 3306) or die("Fail connecting to DB");
-        mysqli_set_charset($con, 'utf8');
-        return $con;
+        $options = [
+            PDO::ATTR_EMULATE_PREPARES => false, // turn off emulation mode for "real" prepared statements
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //make the default fetch be an associative array
+        ];
+
+        try {
+            return new PDO("mysql:host={$_ENV["DB_HOST"]};port={$_ENV["DB_PORT"]};dbname={$_ENV["DB_NAME"]};charset=utf8", $_ENV["DB_USER"], $_ENV["DB_PASS"], $options);
+        } catch (Exception $e) {
+            die("Error connecting database - {$e->getMessage()}");
+        }
     }
 
     /**
@@ -34,9 +40,9 @@ abstract class Connection
 
     public abstract function list(string $search = null): ?array;
 
-    public abstract function create(object $model): bool;
+    public abstract function create($object): bool;
 
-    public abstract function update(object $model): bool;
+    public abstract function update($object): bool;
 
     public abstract function delete(int $id): bool;
 }
